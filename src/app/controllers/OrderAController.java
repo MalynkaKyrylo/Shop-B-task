155a67057baf68a36825026ecf3c44061933012c
Malynka Kyrylo <kirillmalinka@gmail.com>
2023-06-26 21:51:09 +0300

    Correct OrderAController

--- src/app/controllers/OrderAController.java
+++ src/app/controllers/OrderAController.java
@@ -6,33 +6,30 @@ import app.utils.Constants;
 import app.utils.Rounder;
 import app.views.OrderView;
 
-public class OrderAController implements {
+public class OrderAController implements BaseController{
 
     private OrderView view;
     private OrderAModel model;
-    private String data;
+    private String[] data;
     private String costRounded;
 
     @Override
     public void handleData() {
         view = new OrderView();
-        data = getData();
+        data = view.getData();
         model = new OrderAModel();
-        costRounded = Rounder.roundValue();
+        double orderCost = getOrderCost();
+        costRounded = Rounder.roundValue(orderCost);
         view.getOutput(formOutput());
-    }
-
-    private String[] getData() {
-
     }
 
     private double getOrderCost() {
         return model.getOrderCost(Integer.parseInt(data[1]),
-                Double.parseDouble(data[0]));
+                Double.parseDouble(data[2]));
     }
 
     private String formOutput() {
-        return "\nOrder " + data[0] + " cost is " + Constants. +
+        return "\nOrder " + data[0] + " cost is " + Constants.CURRENCY +
                 " " + costRounded;
     }
 }
